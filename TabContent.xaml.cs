d22c817e3f3a2257639cd87664d36215b544d0c1
DATArchitects NET <79222806+FBanuet@users.noreply.github.com>
2022-10-20 00:03:12 -0500

    Add files via upload

---
+++ TabContent.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebBrowserWPF
+{
+    /// <summary>
+    /// Lógica de interacción para TabContent.xaml
+    /// </summary>
+    public partial class TabContent : UserControl
+    {
+        public TabContent()
+        {
+            InitializeComponent();
+        }
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+
+        public static readonly DependencyProperty UrlProperty = DependencyProperty.Register("Url", typeof(string), typeof(TabContent),
+           new PropertyMetadata("http://127.0.0.1:5500/ifcdatasample/main.html"));
+
+        private void Browser_Initialized(object sender, EventArgs e)
+        {
+            Browser.Address = Url;
+        }
+    }
+}
